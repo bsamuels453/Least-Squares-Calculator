eff0e950a0e13a98474ce545d811a8cc2a5fd6d0
Ben Samuels <bsamuels453@gmail.com>
2012-10-06 10:20:14 -0700

    adding ParseFileToNumList implementation

--- Least-Squares-Calculator/Program.cs
+++ Least-Squares-Calculator/Program.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Least_Squares_Calculator {
     class Program {
         static void Main(string[] args) {
+            
+
+
+
+        }
+
+
+        static List<double> ParseFileToNumList(string filename){
+            var reader = new StreamReader(filename);
+            var retList = new List<double>();
+
+            string line;
+            while ((line = reader.ReadLine()) != null){
+                double d;
+
+                if (!double.TryParse(line, out d)){
+                    break;
+                }
+                retList.Add(d);
+            }
+            reader.Close();
+
+            return retList;
         }
     }
 }
