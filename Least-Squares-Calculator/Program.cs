4c87df4e36909a9325529f89437b882afcc36225
Ben Samuels <bsamuels453@gmail.com>
2012-10-06 10:26:08 -0700

    implementing summation methods

--- Least-Squares-Calculator/Program.cs
+++ Least-Squares-Calculator/Program.cs
@@ -7,7 +7,8 @@ using System.Text;
 namespace Least_Squares_Calculator {
     class Program {
         static void Main(string[] args) {
-            
+            var ind = ParseFileToNumList("independents.txt");
+            var de = ParseFileToNumList("dependents.txt");
 
 
 
@@ -30,6 +31,30 @@ namespace Least_Squares_Calculator {
             reader.Close();
 
             return retList;
+        }
+
+        static double Summation(IEnumerable<double> li){
+            double result = 0;
+            foreach (var d in li){
+                result += d;
+            }
+            return result;
+        }
+
+        static double SummationSquared(IEnumerable<double> li){
+            double result = 0;
+            foreach (var d in li){
+                result += d * d;
+            }
+            return result;
+        }
+
+        static double SummationMulti(IList<double> li1, IList<double> li2){
+            double result = 0;
+            for (int i = 0; i < li1.Count(); i++){
+                result += li1[i] * li2[i];
+            }
+            return result;
         }
     }
 }
