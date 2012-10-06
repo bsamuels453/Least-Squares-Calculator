9915090cde24443441966ca527e960a3dd2e7d62
Ben Samuels <bsamuels453@gmail.com>
2012-10-06 10:42:33 -0700

    implemented the least squares fit formula

--- Least-Squares-Calculator/Program.cs
+++ Least-Squares-Calculator/Program.cs
@@ -9,8 +9,27 @@ namespace Least_Squares_Calculator {
         static void Main(string[] args) {
             var ind = ParseFileToNumList("independents.txt");
             var de = ParseFileToNumList("dependents.txt");
+            var uncertain = ParseFileToNumList("uncertainties.txt");
 
+            if (ind.Count != de.Count)
+                throw new Exception("there are a different number of independent and dependent values");
 
+            if (ind.Count != uncertain.Count)
+                throw new Exception("there are a different number of values and uncertanties");
+
+            int n = ind.Count;
+
+            var indSummation = Summation(ind);
+            var deSummation = Summation(de);
+
+            var uncertainSumSquare = SummationSquared(uncertain);
+
+            var indSumSquare = SummationSquared(ind);
+            var crossSum = SummationMulti(ind, de);
+
+            var lssResult = CalculateLeastSquaresFit(n, indSummation, deSummation, indSumSquare, crossSum, uncertainSumSquare);
+
+            int f = 5;
 
         }
 
@@ -55,6 +74,25 @@ namespace Least_Squares_Calculator {
                 result += li1[i] * li2[i];
             }
             return result;
+        }
+
+        static LSFResults CalculateLeastSquaresFit(int n, double indSummation, double deSummation, double indSumSquare, double crossSum, double uncertSumSquare){
+            var retStruct = new LSFResults();
+            retStruct.Slope = (n * crossSum - indSummation * deSummation) / (n * indSumSquare - Math.Pow(indSummation, 2));
+            retStruct.YIntercept = (indSumSquare * deSummation - indSummation * crossSum) / (n * indSumSquare - Math.Pow(indSummation, 2));
+
+            retStruct.Sy = Math.Sqrt(uncertSumSquare / (n - 2));
+            retStruct.Sb = retStruct.Sy * Math.Sqrt(indSumSquare / (n * indSumSquare - Math.Pow(indSummation, 2)));
+            retStruct.Sm = retStruct.Sy * Math.Sqrt(n / (n * indSumSquare - Math.Pow(indSummation, 2)));
+            return retStruct;
+        }
+
+        struct LSFResults{
+            public double Sy;
+            public double Sm;
+            public double Sb;
+            public double Slope;
+            public double YIntercept;
         }
     }
 }
