7dfc8a3ebcfae1dbd37164e5c0e0dd68090552fb
Ben Samuels <bsamuels453@gmail.com>
2012-12-08 10:50:56 -0800

    updating license information

--- Least-Squares-Calculator/LSFForm.Designer.cs
+++ Least-Squares-Calculator/LSFForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (C) 2012 zalzane
+Copyright (C) 2012 Ben Samuels
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
